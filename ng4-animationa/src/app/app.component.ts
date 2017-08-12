2049ce6e1dab9523c6be6ad32148b08f9dc5229b
Danielte <daniel.t.e.n@hotmail.com>
2017-08-12 04:00:59 -0300

    atualizando o angular

--- ng4-animationa/src/app/app.component.ts
+++ ng4-animationa/src/app/app.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
-
+import { trigger } from "@angular/animations";
+  
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('redimencionar', [
+
+    ])
+  ]
 })
 export class AppComponent {
   title = 'app works!';
