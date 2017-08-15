e94ddc9f5e2f645c24e119753fc6bd889bb145aa
Danielte <daniel.t.e.n@hotmail.com>
2017-08-15 01:23:51 -0300

    adicionado o componente para animação de rota

--- ng4-animations/src/app/app.module.ts
+++ ng4-animations/src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { AnimationComponent } from './animation/animation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AnimationComponent
   ],
   imports: [
     BrowserModule,
